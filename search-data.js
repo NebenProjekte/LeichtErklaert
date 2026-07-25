5c1853c2d3ad2b6ae4414e7df1bb484f23c340c1
NebenProjekte <info@drinkexpress.es>
2026-07-25 20:11:08 +0200

    Add LE_TOPICS array with educational content

---
+++ search-data.js
@@ -0,0 +1,14 @@
+var LE_TOPICS = [
+  { title: "Satz des Pythagoras leicht erklaert", cat: "Mathematik", path: "mathematik/satz-des-pythagoras.html", keywords: "pythagoras dreieck hypotenuse kathete geometrie rechtwinklig" },
+  { title: "Brueche leicht erklaert", cat: "Mathematik", path: "mathematik/brueche.html", keywords: "bruchrechnung zaehler nenner bruch addieren" },
+  { title: "Primzahlen leicht erklaert", cat: "Mathematik", path: "mathematik/primzahlen.html", keywords: "primzahl zahlentheorie teilbar" },
+  { title: "Das Drei-Tueren-Problem (Monty Hall) leicht erklaert", cat: "Mathematik", path: "mathematik/drei-tueren-problem.html", keywords: "monty hall ziegenproblem wahrscheinlichkeit stochastik tueren" },
+  { title: "Warum eine abgefeuerte Kugel genauso schnell landet wie eine fallengelassene", cat: "Physik", path: "physik/gravitation-abschuss.html", keywords: "gravitation schwerkraft wurf abschuss kugel" },
+  { title: "Fallgeschwindigkeit leicht erklaert", cat: "Physik", path: "physik/fallgeschwindigkeit.html", keywords: "freier fall geschwindigkeit fallen" },
+  { title: "Ebbe und Flut leicht erklaert", cat: "Physik", path: "physik/ebbe-und-flut.html", keywords: "gezeiten mond ebbe flut" },
+  { title: "Warum die Erde eine Kugel ist und keine Scheibe", cat: "Physik", path: "physik/erde-ist-eine-kugel.html", keywords: "erde kugel scheibe astronomie rund" },
+  { title: "Unterschied zwischen Volt, Ampere und Watt", cat: "Physik", path: "physik/volt-ampere-watt.html", keywords: "volt ampere watt strom elektrizitaet spannung stromstaerke leistung elektro" },
+  { title: "Masse und Dichte leicht erklaert", cat: "Physik", path: "physik/masse-und-dichte.html", keywords: "masse dichte gewicht volumen schwimmen sinken" },
+  { title: "Hebelwirkung (Hebelgesetz) leicht erklaert", cat: "Physik", path: "physik/hebelwirkung.html", keywords: "hebel hebelgesetz wippe kraftarm lastarm" },
+  { title: "Zentripetalkraft und Zentrifugalkraft leicht erklaert", cat: "Physik", path: "physik/zentripetalkraft-zentrifugalkraft.html", keywords: "zentripetalkraft zentrifugalkraft kreisbewegung scheinkraft kurve" }
+];
