de3671f321cfbbd615c2ff65da495603aeceacfe
NebenProjekte <info@drinkexpress.es>
2026-07-26 19:41:20 +0200

    Add LE_TOPICS variable with Pythagorean theorem data

---
+++ search-data.en.js
@@ -0,0 +1,3 @@
+var LE_TOPICS = [
+  { title: "Pythagorean Theorem Explained Simply", cat: "Math", path: "mathematik/satz-des-pythagoras.html", keywords: "pythagorean theorem triangle hypotenuse leg geometry right angle" }
+];
