18f35e3d97708a57c0846dfc0f02a03250e7704d
NebenProjekte <info@drinkexpress.es>
2026-07-26 17:41:40 +0200

    Add unique card order explanation to search data

--- search-data.js
+++ search-data.js
@@ -3,6 +3,7 @@ var LE_TOPICS = [
   { title: "Brueche leicht erklaert", cat: "Mathematik", path: "mathematik/brueche.html", keywords: "bruchrechnung zaehler nenner bruch addieren" },
   { title: "Primzahlen leicht erklaert", cat: "Mathematik", path: "mathematik/primzahlen.html", keywords: "primzahl zahlentheorie teilbar" },
   { title: "Das Drei-Tueren-Problem (Monty Hall) leicht erklaert", cat: "Mathematik", path: "mathematik/drei-tueren-problem.html", keywords: "monty hall ziegenproblem wahrscheinlichkeit stochastik tueren" },
+  { title: "Warum ist jede gemischte Kartenreihenfolge einzigartig", cat: "Mathematik", path: "mathematik/kartenspiel-wahrscheinlichkeit.html", keywords: "kartenspiel karten mischen fakultaet 52 wahrscheinlichkeit stochastik" },
   { title: "Warum eine abgefeuerte Kugel genauso schnell landet wie eine fallengelassene", cat: "Physik", path: "physik/gravitation-abschuss.html", keywords: "gravitation schwerkraft wurf abschuss kugel" },
   { title: "Fallgeschwindigkeit leicht erklaert", cat: "Physik", path: "physik/fallgeschwindigkeit.html", keywords: "freier fall geschwindigkeit fallen" },
   { title: "Ebbe und Flut leicht erklaert", cat: "Physik", path: "physik/ebbe-und-flut.html", keywords: "gezeiten mond ebbe flut" },
