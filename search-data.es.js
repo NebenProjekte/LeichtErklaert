68eaf63db0dc73bf9173e06c328989d1bf409846
NebenProjekte <info@drinkexpress.es>
2026-07-26 19:42:02 +0200

    Add LE_TOPICS variable with Pythagorean theorem data

---
+++ search-data.es.js
@@ -0,0 +1,3 @@
+var LE_TOPICS = [
+  { title: "El Teorema de Pitágoras explicado de forma sencilla", cat: "Matemáticas", path: "mathematik/satz-des-pythagoras.html", keywords: "teorema pitagoras triangulo hipotenusa cateto geometria angulo recto" }
+];
